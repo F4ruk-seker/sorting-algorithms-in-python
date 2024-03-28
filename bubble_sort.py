a8bc6c1da5f02455eb0cd1985055192c93a14d45
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-03-28 22:22:45 +0300

    add BubbleSortTyper

--- bubble_sort.py
+++ bubble_sort.py
@@ -7,7 +7,7 @@ linkedin:https://www.linkedin.com/in/faruk-seker/
 github:https://github.com/F4ruk-seker
 
 """
-from typing import Any
+from typing import Any, Union
 
 
 class BubbleSort:
@@ -28,24 +28,23 @@ class BubbleSort:
 
 
 class BubbleSortTyper(BubbleSort):
+    @staticmethod
+    def str_to_number(obj: str):
+        if obj.replace(',', '').replace('.', '').isnumeric():
+            obj = obj.replace(',', '.')
+            return int(obj) if float(obj).is_integer() else float(obj)
+        return len(obj)
+
+    def to_type(self, obj: Any) -> Union[int, float]:
+        obj: Any = self.str_to_number(obj) if type(obj) is str else obj
+        obj: Any = int(obj) if type(obj) is bool else obj
+        return obj
 
     def sort(self) -> None:
         for first_index in range(len(self.list) - 1, 0, -1):
             for second_index in range(first_index):
-                first_case: Any = self.list[second_index]
-                second_case: Any = self.list[second_index + 1]
-
-                # str
-                if type(first_case) is str:
-                    first_case = len(first_case)
-                if type(second_case) is str:
-                    second_case = len(second_case)
-
-                # bool
-                if type(first_case) is bool:
-                    first_case = int(first_case)
-                if type(second_case) is bool:
-                    second_case = int(second_case)
+                first_case: Any = self.to_type(self.list[second_index])
+                second_case: Any = self.to_type(self.list[second_index + 1])
 
                 if first_case > second_case:
                     _ = self.list[second_index]
@@ -56,13 +55,13 @@ class BubbleSortTyper(BubbleSort):
 if __name__ == '__main__':
     import random
     random.seed(100)
-    test_case = [random.randint(0, 100) for _ in list(range(0, 50))]
+    test_case = [random.randint(0, 100) for _ in list(range(0, 30))]
     bubble_sort: BubbleSort = BubbleSort(test_case)
 
     result: list[int] = bubble_sort.get_result()
     print(result)
 
-    test_case += [True, False, True, '15a1', 'pars', 'parsS']
+    test_case += [True, False, True, '15a1', 'pars', 'parsS', '1453', '1453.50', '1453,55', 'f4ruk.seker']
     bubble_sort_other_objects: BubbleSort = BubbleSortTyper(test_case)
 
     result: list[Any] = bubble_sort_other_objects.get_result()
