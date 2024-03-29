e76cc510c98e354ab6abb7c4698d37c2aed97c33
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-03-29 05:49:52 +0300

    Change Test case to SelectionSort

--- selection_sort.py
+++ selection_sort.py
@@ -7,30 +7,40 @@ from typing import NoReturn
 
 class SelectionSort:
     def __init__(self, _list: list):
-        self.list: list = _list
+        self.__list: list = _list
     
     def sort(self) -> NoReturn:
-        for first_index in range(len(self.list)):
+        for first_index in range(len(self.__list)):
             min_index: int = first_index
-            for second_index in range(first_index + 1, len(self.list)):
-                if self.list[second_index] < self.list[min_index]:
+            for second_index in range(first_index + 1, len(self.__list)):
+                if self.__list[second_index] < self.__list[min_index]:
                     min_index: int = second_index
-            self.list[first_index], self.list[min_index] = self.list[min_index], self.list[first_index]
+            self.__list[first_index], self.__list[min_index] = self.__list[min_index], self.__list[first_index]
 
     @property
     def result(self) -> list:
         self.sort()
-        return self.list
+        return self.__list
 
 
-if __name__ == '__main__':
+def main() -> None:
     import random
 
-    random.seed(100)
-    test_case = [random.randint(0, 100) for _ in list(range(0, 20))]
+    test_case = [random.randint(0, 200) for _ in list(range(0, 20))]
 
     selection_sort: SelectionSort = SelectionSort(test_case)
     result: list = selection_sort.result
 
-    print(f"""\n{'TEST-CASE: '.center(14)}{test_case}\n{'  RESULT: '.center(14)}{result}""")
+    print(f"""| {'TEST-CASE:'.center(10)}|  {test_case}\n|{'   RESULT: '.center(10)}|  {result}""")
+
+    del result
+    del selection_sort
+
+
+if __name__ == '__main__':
+    print(f'|{"-" * 110}|')
+    for test_count in range(5):
+        main()
+        print(f'|{"-"*110}|')
+
 
